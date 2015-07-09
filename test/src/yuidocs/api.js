a430c0f2cf20802d33b36d008db7d8cf12d6b934
0ness <nakamura@wonderful.co.jp>
2015-07-09 13:20:06 +0900

    no message

---
+++ test/src/yuidocs/api.js
@@ -0,0 +1,15 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "BreakEvent",
+        "BreakPointOne",
+        "Library",
+        "Planet",
+        "ScrollFire",
+        "ScrollFireDom",
+        "UserInfo"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
